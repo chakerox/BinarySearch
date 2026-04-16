df097a9c2665a873ba94043da2f34b3515841cdb
chakerox <chakerox@gmail.com>
2026-04-16 10:27:27 +0700

    membuat prosedure input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,6 +5,19 @@ int elemen[10];
 int nPanjang;
 int x;
 
+void input(){
+    while(true){
+        cout << "Masukan banyaknya elemen pada array(maks 10):";
+        cin >> nPanjang;
+
+        if(nPanjang <= 10){
+            break;
+        }
+        else{
+            cout<<"\nJumlah elemen tidal boleh lebih dari 10 silakan coba lagi\n";
+        }
+    }
+}
 int main(){
-    
+
 }
